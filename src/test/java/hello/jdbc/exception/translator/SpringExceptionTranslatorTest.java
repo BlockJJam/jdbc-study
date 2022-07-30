7910fcd036f366aa6a9cd7ffd9cc0d5efb371304
blockjjam <blockjjam99@gmail.com>
2022-07-30 22:58:21 +0900

    feat: 스프링 예외 변환기와 예외 추상화를 통해, 예외 변환과 예외 정의에 대한 편리한 방법, 기술 종속성 해결에 대한 방법을 테스트

---
+++ src/test/java/hello/jdbc/exception/translator/SpringExceptionTranslatorTest.java
@@ -0,0 +1,69 @@
+package hello.jdbc.exception.translator;
+
+import hello.jdbc.connection.ConnectionConst;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.BadSqlGrammarException;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+
+import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
+public class SpringExceptionTranslatorTest {
+
+    DataSource dataSource;
+
+    @BeforeEach
+    void init(){
+        dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+
+    }
+
+    /**
+     * 예외 변환기를 사용하지 않고, 직접 예외 코드를 확인한 경우
+     */
+    @Test
+    void sqlExceptionErrorCode(){
+        String sql = "select bad Grammer";
+
+        try{
+            Connection con = dataSource.getConnection();
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            pstmt.executeQuery();
+        }catch( SQLException e){
+            assertThat(e.getErrorCode()).isEqualTo(42122);
+            int errorCode = e.getErrorCode();
+            log.info("errorCode={}", errorCode);
+            log.info("error", e);
+        }
+    }
+
+    @Test
+    void exceptionTranslator(){
+        String sql = "select bad grammer";
+
+        try{
+            Connection con = dataSource.getConnection();
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            pstmt.executeQuery();
+        }catch(SQLException e){
+            assertThat(e.getErrorCode()).isEqualTo(42122);
+
+            SQLErrorCodeSQLExceptionTranslator exTranslator = new SQLErrorCodeSQLExceptionTranslator(dataSource);
+            DataAccessException resultEx = exTranslator.translate("select", sql, e);
+            log.info("resultEx", resultEx);
+            assertThat(resultEx.getClass()).isEqualTo(BadSqlGrammarException.class);
+        }
+    }
+}
